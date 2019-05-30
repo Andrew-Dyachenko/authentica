38edfdd0c1d5c37418add05f1d1a10e0e70833ed
Andrew <north.inhale@gmail.com>
2019-05-30 16:35:07 +0300

    Footer to bottom resize listener

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
+import './assets/js/array.from.polyfill'
 import 'bootstrap/js/dist/dropdown'
 
-document.addEventListener('DOMContentLoaded', () => {
+/**
+ * Добавляет недостоющую высоту блоку .container--main в случае если
+ * недостаточная высота контента и footer не был прижат к низу.
+ * Этот пример актуален для IE11, где не срабатывает свойство flex-grow: 1
+ * для .container--main
+ */
+const footerToBottom = () => {
+	const bodyContainers = document.querySelectorAll('body > .container');
+	const mainContainer = document.querySelector('.container--main');
 
+	const totalHeight = Array.from(bodyContainers)
+		.reduce((reduced, container) =>
+			reduced + container.offsetHeight, 0)
+
+	mainContainer.style.minHeight = totalHeight < window.innerHeight
+		? `${window.innerHeight - totalHeight}px`
+		: 'auto'
+
+	window.addEventListener('resize', () => {
+		mainContainer.style.minHeight = null
+		footerToBottom()
+	})
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	footerToBottom()
 })
